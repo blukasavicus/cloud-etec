6b3eeb645b5087463f08f777abda70a2f58694a4
unknown <Ana_23868@ETEC.FURLAN>
2024-10-08 10:31:37 -0300

    08/10

--- server.js
+++ server.js
@@ -10,33 +10,19 @@ server.use(express.json()) //Possibilidade de usar JSON
 server.use(bodyParser.urlencoded({ extended: true }))
 
 //EXemplo GET
-server.get('/cadastro', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'))
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Adotando-Aumigos/index.html'))
 })
-//Exemplo POST com resposta 401
-server.post('/cadastro', (req, res) => {
-    console.log(req.body)
-    const { email, name } = req.body
-    //CAdastro os dados no banco de dados!
 
-    if (email !== 'muca@email.com') {
-        return res.sendFile(path.join(__dirname, 'views/401.html'))
-    }
-    res.sendFile(path.join(__dirname, 'views/home.html'))
+server.get('/cadastro', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Adotando-Aumigos/cadastro.html'))
 })
-//Exemplo de retorno JSON
-server.get('/pets', (req, res) => {
-    res.send({
-        name: "Meu gato",
-        idade: "4 anos",
-        peso: "300kg"
-    })
+
+server.get('/filhotes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Adotando-Aumigos/filhotes.html'))
 })
-//Middleware para rotas não encontradas!
-server.use(function (req, res, next) {
-    console.log('aqui');
-    return res.sendFile(path.join(__dirname, 'views/404.html'))
-})
+
+
 
 server.listen(3000, () => {
     console.log("servidor no ar...")
